2112cd8d31b66026d8c0b59a7e6ea63302d5a273
Denise Henderson <denise@henderson3.us>
2020-08-05 09:21:35 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
 
   attr_reader :name
-  attr_accessor :mood, :owner
+  attr_accessor :mood
 
   def initialize(name, owner)
     @name = name
+    owner = owner.new(name)
   end
 
 end
