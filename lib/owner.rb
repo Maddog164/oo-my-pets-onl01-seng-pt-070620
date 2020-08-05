6f83936880239716ac18318df9af26d62972dbdd
Denise Henderson <denise@henderson3.us>
2020-08-05 10:18:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,7 +50,7 @@ class Owner
       cat.owner = nil
       cat.mood = "nervous"
     end
-    self.dogs.each do |dog| self.dogs.delete(cat)
+    self.dogs.each do |dog| self.dogs.delete(dog)
       dog.owner = nil
       dog.mood = "nervous"
     end
